39af0433f830bf955c90bd97c0e936d4fc2ec4a7
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-29 16:31:55 -0500

    Created search params variable

--- public/scripts/controllers/nonprofits.controller.js
+++ public/scripts/controllers/nonprofits.controller.js
@@ -1,11 +1,37 @@
 app.controller('NonprofitController', ['DataFactory', '$http' , function(DataFactory, $http) {
   var self = this;
 
-  self.searchObject = {};
-  console.log(self.searchObject);
+  var volunteerResultList = {list: []};
 
-  //accesses information from public API
-    self.skills = DataFactory.skills;
-    self.causes = DataFactory.causes;
+  self.searchObject = {};
+
+  self.searchVolunteers = function(){
+    var params = {
+      skill: self.searchObject.skill.id,
+      cause: self.searchObject.cause.id,
+      morningAvailability: self.searchObject.morning,
+      afternoonAvailability: self.searchObject.afternoon,
+      eveningAvailability: self.searchObject.evening,
+      weekdayAvailability: self.searchObject.weekdays,
+      weekendAvailability: self.searchObject.weekends,
+      openAvailability: self.searchObject.open
+    };
+
+    
+  }; //end of searchVolunteers function
+
+
+    // {{nc.searchObject.skill.id}}
+    // {{nc.searchObject.cause.id}}
+    // {{nc.searchObject.morning}}
+    // {{nc.searchObject.afternoon}}
+    // {{nc.searchObject.evening}}
+    // {{nc.searchObject.weekdays}}
+    // {{nc.searchObject.weekends}}
+    // {{nc.searchObject.open}}
+
+    //accesses information from public API
+      self.skills = DataFactory.skills;
+      self.causes = DataFactory.causes;
 
 }]);
