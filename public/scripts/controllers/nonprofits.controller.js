9954fd8819f5c74577239db671898a274aa4d231
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-29 11:53:37 -0500

    Working on nonprofit search functionality

--- public/scripts/controllers/nonprofits.controller.js
+++ public/scripts/controllers/nonprofits.controller.js
@@ -1,6 +1,10 @@
 app.controller('NonprofitController', ['DataFactory', '$http' , function(DataFactory, $http) {
   var self = this;
 
+  self.searchSkills = {};
+  self.searchCauses = {};
+  self.searchAvailability = {};
+
   //accesses information from public API
     self.skills = DataFactory.skills;
     self.causes = DataFactory.causes;
